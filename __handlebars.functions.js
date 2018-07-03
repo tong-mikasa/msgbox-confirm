ce922dfa346926e4b0a72cb9d539618a004a1b8d
mikasa.tong1st <tong1st@qq.com>
2018-07-03 14:19:36 +0800

    Add files via upload

---
+++ __handlebars.functions.js
@@ -0,0 +1,44 @@
+/**
+ * Functions
+ */
+(function($){
+    $(function(){
+        if (typeof(Handlebars) === 'undefined'){
+            return;
+        }
+
+        Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+            switch (operator) {
+                case '==':
+                    return (v1 == v2) ? options.fn(this) : options.inverse(this);
+                case '!=':
+                    return (v1 != v2) ? options.fn(this) : options.inverse(this);
+                case '===':
+                    return (v1 === v2) ? options.fn(this) : options.inverse(this);
+                case '<':
+                    return (v1 < v2) ? options.fn(this) : options.inverse(this);
+                case '<=':
+                    return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+                case '>':
+                    return (v1 > v2) ? options.fn(this) : options.inverse(this);
+                case '>=':
+                    return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+                case '&&':
+                    return (v1 && v2) ? options.fn(this) : options.inverse(this);
+                case '||':
+                    return (v1 || v2) ? options.fn(this) : options.inverse(this);
+                default:
+                    return options.inverse(this);
+            }
+        });
+
+        Handlebars.registerHelper('compileOR', function (compileVal, v2, v3,options) {
+            if(compileVal == v2 || compileVal == v3)
+            {
+                return options.fn(this);
+            }
+            return options.inverse(this);
+        });
+    });
+})(jQuery);
